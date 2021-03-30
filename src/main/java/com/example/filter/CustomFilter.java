e3dba2eabd6ead68c4a8135aa26726bb17da0cec
ravi185patel <aryanrdp@gmail.com>
2021-03-30 22:20:17 +0530

    Update CustomFilter.java
    Get all header before request process.

--- src/main/java/com/example/filter/CustomFilter.java
+++ src/main/java/com/example/filter/CustomFilter.java
@@ -15,6 +15,15 @@ public class CustomFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         System.out.println("doFilter called init "+servletRequest);
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+
+        System.out.println("Result: " + req.getHeader("x-auth-token"));
+//         chain.doFilter(request, response);
+        Enumeration<String> enumeration = req.getHeaderNames(); // fetch header names
+		while (enumeration.hasMoreElements()) { // print all headers
+			String headerName = enumeration.nextElement();
+			System.out.println("Header Name:" + headerName + ", Header Value:" + req.getHeader(headerName));
+		}
         filterChain.doFilter(servletRequest,servletResponse);
     }
 
